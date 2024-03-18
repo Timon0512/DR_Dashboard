4cb753ed14bc6028be243a2dcee664742526cf34
Timon0512 <140086510+Timon0512@users.noreply.github.com>
2024-03-18 08:17:48 +0100

    Add files via upload
    asjustments to the dr confirmation and retracement calculation. 
    Speed improved by 300%

--- dr_lens.py
+++ dr_lens.py
@@ -1,4 +1,6 @@
+import datetime
 import sys
+import time as t
 import math
 import pandas as pd
 import numpy as np
@@ -27,6 +29,7 @@ def create_dataset(folder):
     data.index = pd.to_datetime(data.index, utc=True)
     data.index = data.index.tz_convert('America/New_York')
 
+
     return data
 
 
@@ -47,106 +50,156 @@ def format_file(file, symbol, header=None):
     df.to_csv(name)
 
 
-def run_symbols(symbol="all", session="dr"):
+def remove_inconsistent_data(df, session):
 
-    def session_calculations(df, session="dr"):
+    # df = pd.read_csv(df, index_col=0, parse_dates=True)
+    # df.index = pd.to_datetime(df.index, utc=True)
+    df["date"] = df.index.date
 
-        if session == "dr":
-            start_time = time(9, 30)
-            end_time = time(10, 30)
-            end_of_session = time(16, 00)
+    df = session_calculations(df, session, only_dr_state=True)
 
-        elif session == "odr":
-            start_time = time(3, 0)
-            end_time = time(4, 00)
-            end_of_session = time(8, 30)
+    df["duplicate"] = df[["open", "high", "low", "close"]].diff().eq(0).all(axis=1)
 
-        elif session == "adr":
-            start_time = time(19, 30)
-            end_time = time(20, 30)
-            end_of_session = time(2, 30)
+    days = df.groupby("date").agg({"duplicate": "sum"})
+    days = days[days.duplicate > 2]
 
-        # NYC AM Session
+    delete_days = np.unique(days.index)
+    print(f"deleted: {len(delete_days)} days due to duplicates in {session} session")
 
-        df["dr_hour"] = df.index.to_series().apply(lambda x: start_time <= x.time() < end_time)
-        df["dr_session"] = df.index.to_series().apply(lambda x: end_time <= x.time() < end_of_session)
+    df = df[~df.date.isin(delete_days)]
+    df = df.drop(columns=['duplicate'], axis=1)
 
-        df["date"] = df.index.date
-        df["time"] = df.index.time
-        # df = df[df.index.weekday.isin([0, 1, 2, 3, 4])]
+    # print(days)
+    # days.to_csv("error_days.csv")
+    # df.to_csv("errors.csv")
 
-        # Needed for correct iDR values
-        df['body_high'] = df[['open', 'close']].max(axis=1)
-        df['body_low'] = df[['open', 'close']].min(axis=1)
+    #df.to_csv("errors.csv")
 
-        # df.to_csv("test.csv")
 
+
+
+    # df["group"] = (df.shift() == df).all(axis=1).cumsum()
+    # df["date"] = df.index.date
+    # df.to_csv("errors.csv")
+    # # Chck for all dates that have to identical rows following each other
+    # #List with errors
+    # day_w_error = np.unique(df[df.group !=0].index.date)
+    # print(f"removed error days {len(day_w_error)}")
+    # #filter out all error days
+    # df = df[~df.date.isin(day_w_error)]
+    #
+    # df = df.drop(["date", "group", "dr_hour", "dr_session"], axis=1)
+
+    return df
+
+
+def session_calculations(df, session="dr", only_dr_state=False):
+
+    if session == "dr":
+        start_time = time(9, 30)
+        end_time = time(10, 30)
+        end_of_session = time(16, 00)
+
+    elif session == "odr":
+        start_time = time(3, 0)
+        end_time = time(4, 00)
+        end_of_session = time(8, 30)
+
+    elif session == "adr":
+        start_time = time(19, 30)
+        end_time = time(20, 30)
+        end_of_session = time(2, 30)
+
+    # NYC AM Session
+
+    df["dr_hour"] = df.index.to_series().apply(lambda x: start_time <= x.time() < end_time)
+    df["dr_session"] = df.index.to_series().apply(lambda x: end_time <= x.time() < end_of_session)
+
+    # Remove all data from df thats not during the session
+    df = df[(df.dr_session == True) | (df.dr_hour == True)].copy()
+
+    if only_dr_state:
         return df
+
+    df["date"] = df.index.date
+    df["time"] = df.index.time
+
+    # Needed for correct iDR values
+    df['body_high'] = df[['open', 'close']].max(axis=1)
+    df['body_low'] = df[['open', 'close']].min(axis=1)
+
+    return df
+
+
+def run_symbols(symbol="all", session="dr"):
 
     def dr_retracement_calc(df, dr_table):
 
-        df = df[df.dr_session]
-        dr_table = dr_table[dr_table.session_close.notna()].copy()
+        for date, row in dr_table.iterrows():
 
-        for date in dr_table.index:
-            confirmation_time = dr_table.loc[date]["breakout_time"]
-            filter_df = df[df.index.date == date]
-            # print(f"{date} with confiramtion time: {confirmation_time} ")
+            # dr_high = row["dr_high"]
+            # dr_low = row["dr_low"]
+            breakout_time = row["breakout_time"]
+            dr_upday = row["dr_upday"]
+            filter_df = df.query("date == @date and time > @breakout_time")
 
-            if (confirmation_time is pd.NaT) and (len(filter_df) == 0):
-                # print(len(filter_df))
+            if pd.isnull(breakout_time):
+                #print(f"No breakout on {date}")
                 continue
+            try:
+                idx_min = filter_df["low"].idxmin()
+                idx_max = filter_df["high"].idxmax()
+                min_time = idx_min.time()
+                max_time = idx_max.time()
+            except ValueError:
+                #print(f"error on date: {date} breakout time: {breakout_time}, upday: {dr_upday}")
+                filter_df = df.query("date == @date and time >= @breakout_time")
+                idx_min = filter_df["low"].idxmin()
+                idx_max = filter_df["high"].idxmax()
+                min_time = idx_min.time()
+                max_time = idx_max.time()
 
-            elif confirmation_time is pd.NaT:
+            #print(f"{date} minumum at {min_time} and max at: {max_time}")
+            if dr_upday:
+                filter_df = filter_df.query("time <= @max_time")
+                idx_min = filter_df["low"].idxmin()
+                idx_max = filter_df["high"].idxmax()
 
-                high_of_day_idx = filter_df["high"].idxmax()
-                low_of_day_idx = filter_df["low"].idxmin()
+                max_retracement_value = filter_df["low"].min()
+                max_retracement_time = idx_min.time()
+                max_expansion_value = filter_df["high"].max()
+                max_expansion_time = idx_max.time()
+
 
             else:
-                filter_df = filter_df[filter_df.index.time > confirmation_time]
+                filter_df = filter_df.query("time <= @min_time")
 
-                high_of_day_idx = filter_df["high"].idxmax()
-                low_of_day_idx = filter_df["low"].idxmin()
 
-            if (not dr_table.loc[date]["dr_upday"]) and (dr_table.loc[date]["closed_below_dr_high"]):
+                idx_min = filter_df["low"].idxmin()
+                idx_max = filter_df["high"].idxmax()
 
-                filter_df = filter_df[filter_df.index <= low_of_day_idx]
+                max_retracement_value = filter_df["high"].max()
+                max_retracement_time = idx_max.time()
+                max_expansion_value = filter_df["low"].min()
+                max_expansion_time = idx_min.time()
 
-                max_retracement_value = max(filter_df["high"])
-                max_expansion_value = min(filter_df["low"])
-                max_retracement_idx = filter_df["high"].idxmax()
-                max_expansion_idx = filter_df["low"].idxmin()
+            dr_table.at[date, "max_expansion_time"] = max_expansion_time
+            dr_table.at[date, "max_retracement_time"] = max_retracement_time
+            dr_table.at[date, "max_expansion_value"] = max_expansion_value
+            dr_table.at[date, "max_retracement_value"] = max_retracement_value
 
-                dr_table.at[date, "max_expansion_time"] = max_expansion_idx.time()
-                dr_table.at[date, "max_retracement_time"] = max_retracement_idx.time()
-                dr_table.at[date, "max_expansion_value"] = max_expansion_value
-                dr_table.at[date, "max_retracement_value"] = max_retracement_value
 
-                # Non Confirmation Day is handled as Long day
-            else:
-                filter_df = filter_df[filter_df.index <= high_of_day_idx]
+        dr_table["retrace_into_dr"] = dr_table.apply(
+            lambda row: True if (row["dr_upday"] and row["max_retracement_value"] < row["dr_high"])
+                                or (not row["dr_upday"] and row["max_retracement_value"] > row["dr_low"]) else False, axis=1)
 
-                max_retracement_value = min(filter_df["low"])
-                max_expansion_value = max(filter_df["high"])
-                max_retracement_idx = filter_df["low"].idxmin()
-                max_expansion_idx = filter_df["high"].idxmax()
-
-                dr_table.at[date, "max_expansion_time"] = max_expansion_idx.time()
-                dr_table.at[date, "max_retracement_time"] = max_retracement_idx.time()
-                dr_table.at[date, "max_expansion_value"] = max_expansion_value
-                dr_table.at[date, "max_retracement_value"] = max_retracement_value
-
-                dr_table["retrace_into_dr"] = dr_table.apply(
-                    lambda row: True if (row["dr_upday"] and row["max_retracement_value"] < row["dr_high"]) or (
-                                not row["dr_upday"] and row["max_retracement_value"] > row["dr_low"]) else False,
-                    axis=1)
-                dr_table["retrace_into_idr"] = dr_table.apply(
-                    lambda row: True if (row["dr_upday"] and row["max_retracement_value"] < row["idr_high"]) or (
-                                not row["dr_upday"] and row["max_retracement_value"] > row["idr_low"]) else False,
-                    axis=1)
+        dr_table["retrace_into_idr"] = dr_table.apply(
+            lambda row: True if (row["dr_upday"] and row["max_retracement_value"] < row["idr_high"])
+                                or (not row["dr_upday"] and row["max_retracement_value"] > row["idr_low"]) else False, axis=1)
 
         dr_table["expansion_window"] = dr_table.apply(create_time_groups, column_name="max_expansion_time", axis=1)
         dr_table["retracement_window"] = dr_table.apply(create_time_groups, column_name="max_retracement_time", axis=1)
+        #dr_table.to_csv("test.csv", sep=";")
 
         return dr_table
 
@@ -186,14 +239,17 @@ def run_symbols(symbol="all", session="dr"):
         dr_table["dr_true_close"] = dr_table["closed_above_dr_high"] ^ dr_table["closed_below_dr_high"]
         dr_table["dr_true_wick"] = dr_table["breached_dr_low"] ^ dr_table["breached_dr_high"]
 
+        #Remove days where dr high == dr low
+        dr_table = dr_table[dr_table.dr_high != dr_table.dr_low]
+
         return dr_table
 
     def calc_fib_levels(dr_table):
 
-        def calc_fib_level(fib_high, fib_low, value, step=0.25):
+        def calc_fib_level(fib_high, fib_low, value, step=0.1):
 
             fib_level = (value - fib_low) / (fib_high - fib_low)
-
+            # print(f"fib h: {fib_high}, low: {fib_low}, expan: {value}")
             if fib_level >= 0:
                 fib_level = step * math.floor(fib_level / step)
             else:
@@ -201,19 +257,27 @@ def run_symbols(symbol="all", session="dr"):
 
             return fib_level
 
-        for date in dr_table.index:
-            fib_low = dr_table.loc[date]["idr_low"]
-            fib_high = dr_table.loc[date]["idr_high"]
-            expansion_value = dr_table.loc[date]["max_expansion_value"]
-            retracement_value = dr_table.loc[date]["max_retracement_value"]
+        for date, row in dr_table.iterrows():
+            fib_low = row["dr_low"]
+            fib_high = row["dr_high"]
+            expansion_value = row["max_expansion_value"]
+            retracement_value = row["max_retracement_value"]
+            open_value = row["dr_open"]
+            close_value = row["dr_close"]
+
+            if pd.isnull(expansion_value):
+                continue
 
             fib_level_expansion = calc_fib_level(fib_high, fib_low, expansion_value, step=0.1)
             fib_level_retracement = calc_fib_level(fib_high, fib_low, retracement_value, step=0.1)
+            fib_level_open = calc_fib_level(fib_high, fib_low, open_value, step=0.1)
+            fib_level_close = calc_fib_level(fib_high, fib_low, close_value, step=0.1)
 
             dr_table.at[date, "expansion_level"] = round(fib_level_expansion, 2)
             dr_table.at[date, "retracement_level"] = round(fib_level_retracement, 2)
+            dr_table.at[date, "opening_level"] = round(fib_level_open, 2)
+            dr_table.at[date, "closing_level"] = round(fib_level_close, 2)
 
-        # dr_table.to_csv("test.csv")
         return dr_table
 
     def create_time_groups(row, column_name):
@@ -229,13 +293,18 @@ def run_symbols(symbol="all", session="dr"):
     def dr_confirmation_calc(df, dr_table):
 
         df = df[df.dr_session]
-
         upbreak_dates = dr_table[["dr_high"]].to_dict()
         downbreak_dates = dr_table[["dr_low"]].to_dict()
-
+        #df.to_csv("test2.csv", sep=";")
         for date, value in upbreak_dates["dr_high"].items():
-            print(date)
-            filter_df = df[(df.index.date == date) & (df.close > value)]
+
+            #filter_df = df[(df.index.date == date) & (df.close > value)]
+            query_string = f"date == @date and close > @value"
+            # print(date)
+            # print(query_string)
+            # Filtern des DataFrame basierend auf der Query
+            filter_df = df.query(query_string)
+            # print(filter_df)
             try:
                 up_break = filter_df.index[0]
             except IndexError:
@@ -244,12 +313,16 @@ def run_symbols(symbol="all", session="dr"):
             dr_table.at[date, "up_confirmation"] = up_break
 
         for date, value in downbreak_dates["dr_low"].items():
-            filter_df = df[(df.index.date == date) & (df.close < value)]
+            #filter_df = df[(df.index.date == date) & (df.close < value)]
+            query_string = f"date == @date and close < @value"
+            # Filtern des DataFrame basierend auf der Query
+            filter_df = df.query(query_string)
             try:
                 down_break = filter_df.index[0]
             except IndexError:
                 down_break = pd.NaT
             dr_table.at[date, "down_confirmation"] = down_break
+
 
         # print(dr_table)
         dr_table["breakout_time"] = dr_table[["up_confirmation", "down_confirmation"]].min(axis=1)
@@ -270,8 +343,10 @@ def run_symbols(symbol="all", session="dr"):
         return dr_table
 
     def call_defs(symbol, url, session):
+        start = t.time()
 
         df = create_dataset(url)
+        df = remove_inconsistent_data(df, session)
         df = session_calculations(df, session=session)
         dr = dr_basics(df)
         dr = dr_confirmation_calc(df, dr)
@@ -280,14 +355,22 @@ def run_symbols(symbol="all", session="dr"):
         filename = os.path.join("dr_data", f"{symbol}_{session}.csv")
         dr.to_csv(filename, sep=";")
 
+        print(f"{symbol} {session} took: {(t.time() - start) / 60} minutes")
+
     symbol_dict = {
-        "nq": r"C:\Users\timon\PycharmProjects\Backtesting\data\NQ\5Min",
-        "es": r"C:\Users\timon\PycharmProjects\Backtesting\data\ES\5Min",
-        "ym": r"C:\Users\timon\PycharmProjects\Backtesting\data\YM\5Min",
-        "cl": r"C:\Users\timon\PycharmProjects\Backtesting\data\CL\5Min",
-        "eurusd": r"C:\Users\timon\PycharmProjects\Backtesting\data\EURUSD\5Min",
-        "gbpusd": r"C:\Users\timon\PycharmProjects\Backtesting\data\GBPUSD\5Min",
-        "fdax": r"C:\Users\timon\PycharmProjects\Backtesting\data\FDAX\5Min",
+        "nq": r"C:\Timon\Aktien\data\NQ\5Min",
+        "es": r"C:\Timon\Aktien\data\ES\5Min",
+        "ym": r"C:\Timon\Aktien\data\YM\5Min",
+        "cl": r"C:\Timon\Aktien\data\CL\5Min",
+        "gc": r"C:\Timon\Aktien\data\GC",
+        "eurusd": r"C:\Timon\Aktien\data\EURUSD\5Min",
+        "gbpusd": r"C:\Timon\Aktien\data\GBPUSD\5Min",
+        "fdax": r"C:\Timon\Aktien\data\FDAX\5Min",
+        "eu": r"C:\Timon\Aktien\data\EU",
+        "bp": r"C:\Timon\Aktien\data\BP",
+        "cd": r"C:\Timon\Aktien\data\CD",
+        "jy": r"C:\Timon\Aktien\data\JY",
+
     }
 
     sessions = ["dr", "odr"]
@@ -315,8 +398,7 @@ def run_symbols(symbol="all", session="dr"):
                     call_defs(i, url, x)
 
 
+run_symbols(symbol="all", session="all")
 
-run_symbols(symbol="es", session="dr")
 
 
-#format_file(r"C:\Users\timon\PycharmProjects\Backtesting\data\ES.txt", symbol="ES")
